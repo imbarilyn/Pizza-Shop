10a5b6e4b9c0cd77203bd36e19305d27fdca9334
linah.muhonja <linahmuhonjaimbari@gmail.com>
2022-12-10 09:40:33 +0300

    @docs add SignupForm component

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import Error from '../pages/Error'
+
+
+
+function SignupForm({ onLogin }) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordConfirmation, setPasswordConfirmation] = useState("")
+    const [errors, setErrors] =useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setErrors([])
+        setIsLoading(true);
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username, password, password_confirmation: passwordConfirmation}) 
+            
+        }).then(resp=>{
+            setIsLoading(false);
+            if(resp.ok){
+                resp.json().then(user=> onLogin(user));
+            }
+            else{
+                resp.json().then(e => console.log(e.value))
+            }
+        });
+    }
+
+
+
+  return (
+    <div className='wrapper' >
+    <form class="container" onSubmit={handleSubmit}>
+      <p class="heading">Sign up</p>
+      <div class="box">
+        <p>Username</p>
+        <div>
+          <input 
+            type="text"
+            id = "username"
+            autocomplete="off"
+            value = {username}
+            placeholder="Enter your username"
+            onChange={(e) =>setUsername(e.target.value)}
+          /> 
+        </div>
+      </div>
+      <div class="box">
+        <p>Password</p>
+        <div>
+          <input 
+            type="password"
+            id="password"
+            autocomplete="off"
+            value={password}
+            placeholder="Enter your password"
+            onChange = {(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </div>
+      <div class="box">
+        <p>Confirm Password</p>
+        <div>
+          <input 
+            type="password"
+            id="passwor_confirmation"
+            autocomplete="off"
+            value={passwordConfirmation}
+            placeholder="Confirm your password"
+            onChange = {(e) => setPasswordConfirmation(e.target.value)}
+          />
+        </div>
+      </div>
+      <button type="submit" className = "loginBtn">{isLoading? "isLoading...": "Signup"}</button>
+      
+      
+    </form>       
+  </div> 
+  )
+}
+
+export default SignupForm
