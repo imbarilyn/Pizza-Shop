9586e7c14cf5d9a31dd40e9843a7f1ae4cfe41ff
linah.muhonja <linahmuhonjaimbari@gmail.com>
2022-12-10 09:35:02 +0300

    @docs aad the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from 'react'
+import { Routes, Route } from "react-router-dom";
+import './styles/App.css'
+import Announcement from './components/Announcement'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import LoginForm from './components/LoginForm'
+import Login from './pages/Login'
+import Menu from './pages/Menu'
+import SignupForm from './components/SignupForm'
 
 function App() {
+  const [user,setUser] = useState(null)
+
+  useEffect(()=>{
+    fetch("/me").then((resp) => {
+      if (resp.ok) {
+        resp.json().then(user => setUser(user))
+      }
+    });
+  }, []);
+
+  if (!user){
+    <Login onLogin = {setUser} />
+  }
+
   return (
-    <div className="App">
+    <div>
+      <Announcement />
+      <NavBar />
       
+      <Routes>
+        <Route path='/' element = {<Home />}/>
+        <Route path = '/login' element = {<LoginForm />} />
+        <Route path='/menu' element ={<Menu />} />
+        <Route path='/sign_up' element={<SignupForm />}/>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
