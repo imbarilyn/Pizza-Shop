03cc29cff3f63c84a76406deca41e26dc91f270e
linah.muhonja <linahmuhonjaimbari@gmail.com>
2022-12-10 09:54:47 +0300

    @docs add Login.js

---
+++ client/src/pages/Login.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import LoginForm from '../components/LoginForm'
+
+function Login({ onLogin }) {
+
+    const [isLoggIn, setLoggIn] = useState(true)
+  return (
+    
+    <div>
+     
+
+
+    </div>
+  )
+}
+
+export default Login
